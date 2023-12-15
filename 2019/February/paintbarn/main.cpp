4d9e2138382b60d6ec34d273c4a00bd63209de08
alantao5056 <alantaolr@outlook.com>
2023-12-14 20:58:52 -0500

    finished paintbarn

--- 2019/February/paintbarn/main.cpp
+++ 2019/February/paintbarn/main.cpp
@@ -28,37 +28,160 @@ int main() {
     freopen("paintbarn.out", "w", stdout);
 
     cin >> N >> K;
-    vector<vi> prefix(206, vi(206));
+    vector<vi> prefix(202, vi(202));
+
     f(i, 0, N)
     {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        x1+=5; y1+=5; x2+=5; y2+=5;
+        x1+=1; y1+=1; x2+=1; y2+=1;
+
         prefix[x1][y1]++;
-        prefix[x1][y2+1]--;
-        prefix[x2+1][y1]--;
-        prefix[x2+1][y2+1]++;
+        prefix[x1][y2]--;
+        prefix[x2][y1]--;
+        prefix[x2][y2]++;
     }
 
-    vector<vi> barn(206, vi(206));
-
-    f(i, 1, 206)
+    vector<vi> barn(201, vi(201));
+    vector<vi> ones(201, vi(201));
+    vector<vi> ones2(201, vi(201));
+    int tot = 0;
+    f(i, 1, 201)
     {
-        f(j, 1, 206)
+        f(j, 1, 201)
         {
             barn[i][j] = prefix[i][j] + barn[i-1][j] + barn[i][j-1] - barn[i-1][j-1];
+            if (barn[i][j] == K-1)
+            {
+                ones[i][j] = 1;
+            } else if (barn[i][j] == K)
+            {
+                ones[i][j] = -1;
+                tot++;
+            }
+            ones2[i][j] = ones[i][j];
+            ones2[i][j] += ones2[i][j-1];
 //            cout << barn[i][j] << " ";
         }
+
 //        cout << endl;
     }
 
-    f(i, 1, 206)
+    int result = -100000;
+    int x3 = 0;
+    int y3 = 0;
+    int x4 = 0;
+    int y4 = 0;
+    f(i, 1, 201)
     {
-        f(j, 1, 206)
+        f(j, i, 201)
         {
+            // kadane's algorithm
+            vector<int> vertical(201);
+            int maxCover = -100000;
+            int endHere = 0;
+            int s = 1;
+            int s2 = 1;
+            int e = 1;
+            f(k, 1, 201)
+            {
+                vertical[k] = ones2[k][j] - ones2[k][i-1];
+                endHere += vertical[k];
+                if (endHere > maxCover)
+                {
+                    e = k;
+                    s2 = s;
+                    maxCover = endHere;
+                }
+                if (endHere < 0)
+                {
+                    s = k+1;
+                    endHere = 0;
+                }
+            }
 
+            if (maxCover > result)
+            {
+                x3 = s2;
+                y3 = i;
+                x4 = e;
+                y4 = j;
+                result = maxCover;
+            }
         }
     }
+
+    if (result <= 0)
+    {
+        cout << tot << endl;
+        return 0;
+    }
+
+    tot += result;
+
+    // cleanup
+    cf(i, x3, x4)
+    {
+        cf(j, y3, y4)
+        {
+            ones[i][j] = -100000;
+        }
+    }
+
+    f(i, 1, 201)
+    {
+        f(j, 1, 201)
+        {
+            ones2[i][j] = ones[i][j];
+            ones2[i][j] += ones2[i][j-1];
+        }
+    }
+
+    result = -100000;
+    x3 = 0;
+    y3 = 0;
+    x4 = 0;
+    y4 = 0;
+    f(i, 1, 201)
+    {
+        f(j, i, 201)
+        {
+            // kadane's algorithm
+            vector<int> vertical(201);
+            int maxCover = -100000;
+            int endHere = 0;
+            int s = 1;
+            int s2 = 1;
+            int e = 1;
+            f(k, 1, 201)
+            {
+                vertical[k] = ones2[k][j] - ones2[k][i-1];
+                endHere += vertical[k];
+                if (endHere > maxCover)
+                {
+                    e = k;
+                    s2 = s;
+                    maxCover = endHere;
+                }
+                if (endHere < 0)
+                {
+                    s = k+1;
+                    endHere = 0;
+                }
+            }
+
+            if (maxCover > result)
+            {
+                x3 = s2;
+                y3 = i;
+                x4 = e;
+                y4 = j;
+                result = maxCover;
+            }
+        }
+    }
+
+    cout << max(tot, tot + result) << endl;
 
     return 0;
 }
